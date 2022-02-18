2c05179c948d8e061a82f3f198914b946351f491
martinbolsnes <martinbolsnes@icloud.com>
2022-02-18 12:05:16 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+type Task = {
+  id: number | string;
+  title: string;
+};
+
 function App() {
+  const initialTasks: Task[] = [
+    {
+      id: 1,
+      title: 'Task 1',
+    },
+    {
+      id: 2,
+      title: 'Task 2',
+    },
+  ];
+
+  const [taskValue, setTaskValue] = useState('');
+  const [taskList, setTaskList] = useState(initialTasks);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Simple Todo List</h1>
+
+        <div>
+          <form
+            onSubmit={(event) => {
+              event.preventDefault();
+              setTaskList([
+                ...taskList,
+                {
+                  id: Math.random(),
+                  title: taskValue,
+                },
+              ]);
+            }}
+          >
+            <input
+              value={taskValue}
+              onChange={(event) => setTaskValue(event.target.value)}
+            ></input>
+            <br />
+            <button type='submit'>Add</button>
+          </form>
+        </div>
+        <section>
+          {taskList.map((task) => {
+            return (
+              <div
+                key={task.id}
+                onClick={() => {
+                  const newTaskList = taskList.filter((currentTask) => {
+                    if (currentTask.id === task.id) {
+                      return false;
+                    } else {
+                      return true;
+                    }
+                  });
+
+                  setTaskList(newTaskList);
+                }}
+              >
+                <p>{task.title}</p>
+                <hr />
+              </div>
+            );
+          })}
+        </section>
       </header>
     </div>
   );
